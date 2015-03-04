e0fe767082e2f17fbb5bc68da68769adf8494501
Pablo Cardenas <pcardenasoliveros@gmail.com>
2015-03-03 19:54:06 -0600

    Changed users enrollment to VARCHAR(60)

--- db/migrate/20150221005910_create_users.rb
+++ db/migrate/20150221005910_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name, limit: 50, null: false
       t.string :last_names, limit: 60, null: false
-      t.string :enrollment, limit: 9, null: false
+      t.string :enrollment, limit: 60, null: false
       t.integer :major, limit: 3, null: false
       t.integer :campus, null: false
       t.string :hashed_password, limit: 128, null: false
